5e01a21e147650ba4b1881c995cbd8ec4f9bac12
Bablu Bambal <48877691+bablubambal@users.noreply.github.com>
2022-03-15 08:30:27 +0400

    Add files via upload

---
+++ Day-23 Intro Js/index.js
@@ -0,0 +1,98 @@
+console.log("Hello World")
+//How to print in js
+console.log("hi i am printing on console")
+//Warning:
+console.warn("hi i am a warning")
+//Info:
+console.info("i am info of conosle")
+
+
+///////Comments in Js:
+ console.log( "2 types of Comments \n 1. Single line \n 2 .multi line comments ")
+// Single line commment
+ /*Multi 
+line 
+comment */
+
+// Variables in JS:
+// Varible can be declared by using three key words:
+// let
+// const 
+// var
+let k = "bablu"
+var name = "super"
+const myconst = 3;
+a = 20
+b = 30
+a = 10
+a = 45
+// myconst = 4
+c = "myname"
+d = "fullname"
+console.log(a+b)
+console.log(c,d)
+
+// Operators in Js:
+//Arithmetic Opertors:
+// +,-,*,/,%
+a = 10
+b = 20
+console.log("Adding ",a+b)
+console.log(a-b)
+console.log(a*b)
+console.log(a/b)
+console.log(a%b)
+//Comparison operators
+console.log(a,"greater than ",b,a > b)
+console.log(a,"less than ",b,a < b)
+console.log(a==b)
+console.log(a != b)
+
+// >= ,<=, ===, ==
+
+//Assigment operatios ;
+a = 20
+// 20 is assigned to a variable
+
+
+//Loical Operators
+// and && 
+// or || 
+// ! --> js
+x = 5
+y = 0
+console.log(x < 10 && y > 1)
+
+console.log(x< 10 || y> 10)
+boo = true;
+boo = false
+console.log("the boo value is : ", !boo)
+
+//Data type in js:
+/* 
+1. int-py  || number -js 
+2. float
+3. string
+4. boolean
+5. null
+6. undefined
+8  number
+9. bigint
+
+
+*/
+a = 10
+b = 10.5
+c = "String"
+d = true
+console.log(typeof(a))
+console.log(typeof(b))
+console.log(typeof(c))
+console.log(typeof(d))
+
+e = [1,2,3,4]
+f = {1: "one",2:"two"}
+console.log(typeof(e))
+console.log(typeof(f))
+
+/** String Fu    */
